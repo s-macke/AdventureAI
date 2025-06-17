2161ff77a1f373d812d44bb65a00a7274e297f14
Sebastian Macke <sebastian@macke.de>
2025-06-17 20:24:12 +0200

    Switch to new Gemini Lib

--- src/chat/backend/gemini.go
+++ src/chat/backend/gemini.go
@@ -2,61 +2,51 @@ package backend
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"github.com/google/generative-ai-go/genai"
-	"google.golang.org/api/option"
+	"google.golang.org/genai"
 	"os"
-	"strings"
-	"time"
 )
 
-type VertexAIChat struct {
+type GeminiAIChat struct {
 	ctx    context.Context
 	client *genai.Client
-	gemini *genai.GenerativeModel
+	config *genai.GenerateContentConfig
 	model  string
 }
 
-func NewVertexAIChat(systemMsg string, model string) *VertexAIChat {
+func NewVertexAIChat(systemMsg string, model string) *GeminiAIChat {
 	key := os.Getenv("GEMINI_API_KEY")
 	if key == "" {
 		panic("GEMINI_API_KEY env var not set")
 	}
 
 	var err error
-	cs := &VertexAIChat{
-		ctx:   context.Background(),
-		model: model,
+	cs := &GeminiAIChat{
+		ctx: context.Background(),
 	}
-	cs.client, err = genai.NewClient(cs.ctx, option.WithAPIKey(key))
+	cs.client, err = genai.NewClient(cs.ctx, &genai.ClientConfig{
+		APIKey:  key,
+		Backend: genai.BackendGeminiAPI,
+	})
 	if err != nil {
 		panic(err)
 	}
 	/*
-		it := cs.client.ListModels(context.Background())
-		for {
-			model, err := it.Next()
-			if err == nil {
-				fmt.Println(model)
-			} else {
-				break
-			}
+		for key := range cs.client.Models.All(cs.ctx) {
+			fmt.Println(key.Name)
 		}
+		os.Exit(0)
 	*/
-	switch cs.model {
+	switch model {
 	case "gemini-15-pro":
-		cs.gemini = cs.client.GenerativeModel("gemini-1.5-pro-latest")
+		cs.model = "gemini-1.5-pro-latest"
 	case "gemini-15-pro-exp":
-		cs.gemini = cs.client.GenerativeModel("gemini-1.5-pro-exp-0801")
+		cs.model = "gemini-1.5-pro-exp-0801"
 	case "gemini-15-flash":
-		cs.gemini = cs.client.GenerativeModel("gemini-1.5-flash-latest")
+		cs.model = "gemini-1.5-flash-latest"
+	case "gemini-20-flash":
+		cs.model = "gemini-2.0-flash"
 	default:
-		panic("Unknown model")
-	}
-
-	cs.gemini.SystemInstruction = &genai.Content{
-		Parts: []genai.Part{genai.Text(systemMsg)},
+		panic("Unknown model: " + model)
 	}
 
 	safety := []*genai.SafetySetting{
@@ -92,90 +82,55 @@ func NewVertexAIChat(systemMsg string, model string) *VertexAIChat {
 		*/
 		{
 			Category:  genai.HarmCategoryHarassment,
-			Threshold: genai.HarmBlockNone,
+			Threshold: genai.HarmBlockThresholdBlockNone,
 		},
 		{
 			Category:  genai.HarmCategoryHateSpeech,
-			Threshold: genai.HarmBlockNone,
+			Threshold: genai.HarmBlockThresholdBlockNone,
 		},
 		{
 			Category:  genai.HarmCategorySexuallyExplicit,
-			Threshold: genai.HarmBlockNone,
+			Threshold: genai.HarmBlockThresholdBlockNone,
 		},
 		{
 			Category:  genai.HarmCategoryDangerousContent,
-			Threshold: genai.HarmBlockNone,
+			Threshold: genai.HarmBlockThresholdBlockNone,
 		},
 	}
-	cs.gemini.SafetySettings = safety
+
+	cs.config = &genai.GenerateContentConfig{
+		SystemInstruction: &genai.Content{
+			Role:  genai.RoleUser,
+			Parts: []*genai.Part{{Text: systemMsg}},
+		},
+		SafetySettings: safety,
+	}
+
 	return cs
 }
 
-func (cs *VertexAIChat) CallChat(messages []*genai.Content, lastMessage genai.Text) *genai.GenerateContentResponse {
-	var err error
-	var response *genai.GenerateContentResponse
-	for i := 0; i < 60; i++ {
-		chat := cs.gemini.StartChat()
-		chat.History = messages
-		response, err = chat.SendMessage(cs.ctx, lastMessage)
-		if err != nil {
-			fmt.Println("Error:", err)
-			if strings.Contains(err.Error(), "429") {
-				time.Sleep(20 * time.Second)
-				continue
-			}
-			/*
-				if s, ok := status.FromError(err); ok {
-					if s.Code() == 429 {
-						time.Sleep(20 * time.Second)
-						continue
-					}
-				}
-			*/
-			panic(errors.Unwrap(err))
-		}
-		return response
-	}
-	panic(err)
-}
+func (cs *GeminiAIChat) GetResponse(ch *ChatHistory) (string, int, int) {
+	var history []*genai.Content
 
-func (cs *VertexAIChat) GetResponse(ch *ChatHistory) (string, int, int) {
-	var messages []*genai.Content
-
-	if ch.GetLastMessage().Role != "user" {
-		panic("Last message must be from user")
-	}
-
-	lastMessage := genai.Text(ch.GetLastMessage().Content)
-
-	for i, m := range ch.Messages {
-		// Don't add last message
-		if i == len(ch.Messages)-1 {
-			break
-		}
-		messages = append(messages, &genai.Content{
-			Role:  MapVertexAiRole(m.Role),
-			Parts: []genai.Part{genai.Text(m.Content)},
+	for _, m := range ch.Messages {
+		history = append(history, &genai.Content{
+			Role:  string(MapGeminiAiRole(m.Role)),
+			Parts: []*genai.Part{{Text: m.Content}},
 		})
 	}
-	response := cs.CallChat(messages, lastMessage)
-
-	//fmt.Println(response.Candidates[0].Content.Parts[0].(genai.Text))
-
-	// An empty string or just a new line is returned as no text
-	if len(response.Candidates[0].Content.Parts) == 0 {
-		return "", 0, 0
+	response, err := cs.client.Models.GenerateContent(cs.ctx, cs.model, history, cs.config)
+	if err != nil {
+		panic(err)
 	}
-	output := response.Candidates[0].Content.Parts[0].(genai.Text)
-	return string(output), 0, 0
+	return response.Text(), 0, 0
 }
 
-func MapVertexAiRole(role string) string {
+func MapGeminiAiRole(role string) genai.Role {
 	switch role {
 	case ChatHistoryRoleUser:
-		return "user"
+		return genai.RoleUser
 	case ChatHistoryRoleAssistant:
-		return "model"
+		return genai.RoleModel
 	default:
 		panic("Unknown role")
 	}
